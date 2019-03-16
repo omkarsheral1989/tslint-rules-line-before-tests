8637669bb4aa947737b60db45c06493b80a95322
Omkar Sheral <osheral@thoughtworks.com>
2019-03-17 00:41:57 +0530

    [Omkar] [completeLog: takes depth from env variable]

--- src/completeLog.ts
+++ src/completeLog.ts
@@ -3,7 +3,7 @@ const util = require('util');
 export const completeLog = (message?: any, ...optionalParams: any[]): undefined => {
   if (process.env.LOGS === 'true') {
     console.log(message, ...optionalParams.map(
-      (param) => util.inspect(param, false, 4))
+      (param) => util.inspect(param, false, (process.env.LOGS_DEPTH || 2) as number))
     );
   }
   return undefined;
